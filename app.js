15eeea1343adbe1f59c19d2f994bdf1a515d92a2
Jonah Tidwell <908190@H2WHK0YAQ6NW.tld>
2022-08-25 08:08:03 -0500

    082522 REB BULL AHHHHHHH COMMIT

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function sentences(verb , adjective , adverb , person , place) {
 
 var a = [1,2,4,9]
 
-var arr = [
+var wordArr = [
     {
         word: "Max",
         hint: "1x Wordl champion"
@@ -34,11 +34,51 @@ var arr = [
         hint: "7x World Champion"
     },
     {
-        word: "COTA"
+        word: "COTA",
         hint: "F1 track in America"
     },
 ]
 
+var rN= Math.floor(Math.random() * wordArr.length);
+
+var word = wordArr[rN].word;
+var hint = wordArr[rN].hint;
+
+document.getElementById("word").innerHTML = word;
+document.getElementById("hint").innerHTML = hint;
+var w = document.getElementById("word");
+var correct = []
+var counter = word.length + 2
+
+var splitWord = word.split("");
+var emptySplitword = [];
+
+console.log(splitWord)
+
+for (i = 0; i <  splitWord.length; i++) {
+    emptySplitword[i] = "-"
+    console.log(emptySplitword);
+    w.innerHTML = emptySplitword.join(" ")
+}
+
+function enterLetter() {
+    var letter = document.getElementById("letter").value;
+    
+    for (x = 0; x < splitWord.length; x++) {
+        if (letter == splitWord[x]) {
+            emptySplitword[x] = letter;
+            w.innerHTML = emptySplitword.join(" ")
+            document.getElementById("letter").value
+        }else {
+            document.getElementById("letter").value = "";
+        }
+    }
+
+
+    if (correct.length == emptySplitword.length) {
+        alert("die")
+    } else if 
+}
 
 
 
